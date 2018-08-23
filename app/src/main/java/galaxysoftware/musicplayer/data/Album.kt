9f280deb0f794c252172ce1199c4716cc0c4dc70
GINGA WATANABE <galaxygin7250@gmail.com>
2018-08-23 15:06:04 +0900

    Remove files in [data] folder, which changed to [model] folder

--- app/src/main/java/galaxysoftware/musicplayer/data/Album.kt
+++
@@ -1,8 +0,0 @@
-package galaxysoftware.musicplayer.data
-
-import android.graphics.Bitmap
-
-class Album {
-    var title: String? = null
-    var thumbnail: Bitmap? = null
-}
--- app/src/main/java/galaxysoftware/musicplayer/data/Artist.kt
+++
@@ -1,8 +0,0 @@
-package galaxysoftware.musicplayer.data
-
-import android.graphics.Bitmap
-
-class Artist {
-    var title: String? = null
-    var thumbnail: Bitmap? = null
-}
